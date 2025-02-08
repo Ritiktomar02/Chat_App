fdf77a36df78eeb591a61afd043d82104c1c26fc
Ritik Tomar <ritiktomar@Ritiks-MacBook-Air.local>
2025-02-08 14:52:08 +0530

    url changes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOption = {
-    origin: 'http://localhost:3000',
+    origin: 'https://chat-app-b6gl.onrender.com',
     credentials: true
 };
 app.use(cors(corsOption));
